84cef739f52f82f1f89bd7bae40c26ce1a28a0c4
mzzhuo <mzzhuo@gmail.com>
2021-11-09 23:35:39 +0000

    some minor changes

--- pybamm/mz_develop/test_pe_degradation_SPM.py
+++ pybamm/mz_develop/test_pe_degradation_SPM.py
@@ -69,7 +69,7 @@ import matplotlib.pyplot as plt
 import pybamm.mz_develop.output_module as outmod
 
 #%%
-model = pybamm.lithium_ion.SPM({"PE degradation": "none"})
+model = pybamm.lithium_ion.SPM({"PE degradation": "yes"})
 #%
 param = pybamm.ParameterValues(chemistry=pybamm.parameter_sets.Zhuo2021)
 #%%
@@ -77,13 +77,13 @@ experiment = pybamm.Experiment(
     [
         (
             "Charge at 0.5 C until 4.2 V",
-            # "Hold at 4.2 V until C/50",
-            # "Rest for 30 minutes",
-            # "Discharge at 1.0 C until 2.8 V",
-            # "Hold at 2.8 V until C/50",
-            # "Rest for 30 minutes",
+            "Hold at 4.2 V until C/50",
+            "Rest for 60 minutes",
+            "Discharge at 0.1 C until 2.8 V",
+            "Hold at 2.8 V until C/50",
+            "Rest for 60 minutes",
         )
-    ] * 1
+    ] * 20
 )
 
 sim = pybamm.Simulation(
@@ -96,8 +96,20 @@ solution = sim.solve()
 
 #%%
 output_variables = outmod.output_variables_spm
-sim.plot(output_variables)
+sim.plot(output_variables)  
 
+#%%
+total_cycles = len(solution.cycles)
+Q_dis_cycles = []
+cycle_numbers = np.arange(1, total_cycles+1)
+for i in range(total_cycles):
+    Q_dis_cyc = solution.cycles[i].steps[3]["Discharge capacity [A.h]"].entries
+    Q_dis = Q_dis_cyc[-1] - Q_dis_cyc[0]
+    Q_dis_cycles.append(Q_dis)
+    
+plt.figure(figsize=(8, 6))
+# markerfacecolor
+plt.plot(cycle_numbers, Q_dis_cycles, 'o', mfc='none')
  
 
 
